8e09079daf894301c77488b1662280207fca98a0
Dmytro Yemelianov <dmytroyemelianov@icloud.com>
2026-01-13 13:23:46 +0200

    Add custom PerformanceChart component and visualize benchmark data in blog articles

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import { simpleMermaid } from './src/plugins/simple-mermaid.mjs';
 
 export default defineConfig({
   site: 'https://rapscli.xyz',
@@ -9,12 +10,20 @@ export default defineConfig({
   build: {
     format: 'directory',
   },
+  experimental: {
+    contentLayer: true,
+  },
   integrations: [
-    mdx(),
+    mdx({
+      remarkPlugins: [simpleMermaid],
+    }),
     sitemap(),
     tailwind(),
   ],
   markdown: {
+    remarkPlugins: [
+      simpleMermaid,
+    ],
     shikiConfig: {
       theme: 'github-dark',
       wrap: true,
